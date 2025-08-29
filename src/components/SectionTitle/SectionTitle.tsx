2296d8690c214650d859cbe95cea9c1c46967dc6
max <justmax.xex@mail.ru>
2025-08-29 23:33:41 +0200

    refactor: change margin

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,5 +1,5 @@
 import { Text } from "react-native"
 
 export const SectionTitle = ({ children }: { children: React.ReactNode }) => (
-  <Text className="text-gray-500 text-lg mt-4">{children}</Text>
+  <Text className="text-gray-500 text-lg my-2">{children}</Text>
 )
